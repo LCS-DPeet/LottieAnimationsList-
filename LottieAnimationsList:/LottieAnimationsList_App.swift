4095217196543e7b44c14c0bb49d846bc0a32614
Danika Peet <DPeet@lcs.on.ca>
2024-02-03 15:52:51 -0500

    Initial Commit

---
+++ LottieAnimationsList:/LottieAnimationsList_App.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsList_App.swift
+//  LottieAnimationsList:
+//
+//  Created by Danika Peet on 2024-02-03.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsList_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
